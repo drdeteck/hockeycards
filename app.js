55ef9215432184869043d243698d2b69a2c3257f
Philippe Lavoie <phil@philippelavoie.com>
2026-04-30 16:01:25 -0400

    Remove Stickers card grid from menu; binder-only access

--- app.js
+++ app.js
@@ -1971,12 +1971,6 @@ function DataViewModel() {
                     key: marioAll.set_key,
                     displayName: 'All ML Cards'
                 }];
-                if (stickerAllItem) {
-                    allGroupControls.push({
-                        key: stickerAllItem.set_key,
-                        displayName: 'Stickers'
-                    });
-                }
                 orderedMarioGroups.push({
                     text: 'All',
                     controls: allGroupControls
